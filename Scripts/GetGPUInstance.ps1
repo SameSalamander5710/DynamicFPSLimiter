c6c4843828982a31d8752076955270dbae7504a4
SameSalamander5710 <jiffin000@gmail.com>
2025-03-22 16:37:47 +0100

    Add files via upload

---
+++ Scripts/GetGPUInstance.ps1
@@ -0,0 +1,5 @@
+﻿# Use the following to identify the right instance for your GPU utilization
+
+Get-CimInstance Win32_PerfFormattedData_GPUPerformanceCounters_GPUEngine | Select-Object Name, UtilizationPercentage
+
+# Replace the (*_phys_0_eng_0_engtype_3D) with the correct instance if you have multiple GPUs running and its affecting your GPU utilization parameter
---
+++ Scripts/IncreaseLimit.ahk
@@ -0,0 +1,2 @@
+Send "{=}"  ; Simulate pressing =
+
