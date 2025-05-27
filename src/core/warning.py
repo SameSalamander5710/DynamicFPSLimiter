86b633be816407b62a2db76a82e99a2b3af8c789
SameSalamander5710 <jiffin000@gmail.com>
2025-05-27 18:21:14 +0200

    Added dynamic warning for minvalidfps

--- src/core/warning.py
+++ src/core/warning.py
@@ -3,22 +3,32 @@
 def check_min_greater_than_max(dpg, cm):
     mincap = dpg.get_value("input_mincap")
     maxcap = dpg.get_value("input_maxcap")
-    if mincap > maxcap:
-        return "Minimum FPS limit is greater than maximum FPS limit."
+    if mincap >= maxcap:
+        return "[WARNING]: Minimum FPS limit should be less than maximum FPS limit."
     return None
 
 def check_rtss_running(rtss_manager):
     if not rtss_manager or not rtss_manager.is_rtss_running():
-        return "RTSS is not running!"
+        return "[WARNING]: RTSS is not running!"
     return None
 
-def get_active_warnings(dpg, cm, rtss_manager):
+def check_min_greater_than_minvalidfps(dpg, cm, mincap):
+    if mincap < cm.minvalidfps:
+        return "[WARNING]: Minimum FPS limit should be > minimum valid FPS of {}.".format(cm.minvalidfps)
+    return None
+
+def get_active_warnings(dpg, cm, rtss_manager, mincap):
     warnings = []
     # Add more checks as needed
     msg = check_min_greater_than_max(dpg, cm)
     if msg:
         warnings.append(msg)
+
     msg = check_rtss_running(rtss_manager)
+    if msg:
+        warnings.append(msg)
+    
+    msg = check_min_greater_than_minvalidfps(dpg, cm, mincap)
     if msg:
         warnings.append(msg)
     # Add more warning checks here...
